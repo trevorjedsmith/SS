f0b720660528c43486f0af70125161d0b2c5908f
trevor <trevor_jed_smith@hotmail.com>
2017-06-13 18:46:11 +0930

    New Model files

--- SportsStore.Models/Product.cs
+++ SportsStore.Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,14 @@ namespace SportsStore.Models
     public class Product
     {
         public int ProductID { get; set; }
+        [Required(ErrorMessage ="Please enter a name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "Please specify a category")]
         public string Category { get; set; }
     }
 }
